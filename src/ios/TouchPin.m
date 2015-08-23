604eaae62afca7389208159384d5c9aa361263f0
Andreas Pardeike <andreas@pardeike.net>
2015-08-23 16:48:25 +0200

    storing null or empty token erases it

--- src/ios/TouchPin.m
+++ src/ios/TouchPin.m
@@ -5,23 +5,25 @@
 
 - (void)retrievePin:(CDVInvokedUrlCommand*)command {
 	NSString* callbackId = [command callbackId];
-	
+
 	SecItemWrapper *wrapper = [[SecItemWrapper alloc] initWithServiceName:@"TestService"];
 	NSString *pin = [wrapper getSecret];
-	
+
 	CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:pin];
 	[self.commandDelegate sendPluginResult:result callbackId:callbackId];
 }
 
 - (void)storePin:(CDVInvokedUrlCommand*)command {
-	
+
 	NSString* callbackId = [command callbackId];
 	NSString* token      = [[command arguments] objectAtIndex:0];
-	
+
 	SecItemWrapper *wrapper = [[SecItemWrapper alloc] initWithServiceName:@"TestService"];
 	[wrapper deleteSecret];
-	[wrapper addSecret:token];
-	
+	if(token != NULL && [token length] > 0) {
+		[wrapper addSecret:token];
+	}
+
 	CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"ok"];
 	[self.commandDelegate sendPluginResult:result callbackId:callbackId];
 }
